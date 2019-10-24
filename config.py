1751b85f15a305c95fcc8feaeeedc00a9a478119
Ximin Chen <h4nn11113@gmail.com>
2019-10-23 19:28:18 -0700

    set session life time

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,6 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=10)
     TICKETMASTER_API_KEY = 'ekOl1mWCrvka6hMLAxMbRnhHHGbNFFEy'
     HOST = "https://app.ticketmaster.com"
     PATH = "/discovery/v2/events.json"
